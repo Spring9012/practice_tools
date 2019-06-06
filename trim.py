0dc46750c8535126c28a7b108af3c97d95d57ff9
Spring9012 <727955541@qq.com>
2019-06-06 19:29:11 +0800

    qqq

--- trim.py
+++ trim.py
@@ -9,3 +9,14 @@ def is_int(x):
         return True
     else:
         return False
+
+def sum_row(data,col_name=None):
+    '''
+    :param data:
+    :param col_name:
+    :return:指定的列每行的和
+    '''
+    if col_name:
+        return data[col_name].apply(lambda x:sum([i if pd.notnull(i) else 0 for i in x]),axis=1)
+    else:
+        return data.apply(lambda x:sum([i if pd.notnull(i) else 0 for i in x]),axis=1)
